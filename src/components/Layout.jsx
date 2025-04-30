4fa66512f50a7f5ff739c49f0e1c1a90bf4a14a2
Elio Hoyos <elio18david@gmail.com>
2025-04-29 20:21:11 -0500

    layout/navbar

---
+++ src/components/Layout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import { Container, Box } from "@mui/material";
+
+const Layout = () => {
+    return (
+        <>
+            <Navbar />
+            <Container maxWidth="lg">
+                <Box sx={{ my: 4 }}>
+                    <Outlet />
+                </Box>
+            </Container>
+        </>
+    );
+};
+export default Layout;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+
+const Navbar = () => {
+    return (
+        <AppBar position="static">
+            <Toolbar>
+            <Typography variant="h6" component="div" sx={{flexGrow:1}}>
+                Tienda Virtual
+            </Typography>
+            <Button color="inherit" component={Link} to="/categorias">
+                Categorías
+            </Button>
+            <Button color="inherit" component={Link} to="">
+                Productos
+            </Button>
+            </Toolbar>
+        </AppBar>
+    )
+};
+export default Navbar;
